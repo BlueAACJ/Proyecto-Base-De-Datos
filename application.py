65e36debd3fad5e7e3d823a81781936ff1079aaa
BlueAACJ <alejandroxd3820@gmail.com>
2023-11-26 23:45:03 -0600

    CARLOS MAMALA NI VERGA HAZ HECHO Y ESTAS DE FRESCO

--- application.py
+++ application.py
@@ -4,10 +4,9 @@ import pyodbc
 #Conexion Base de Datos TODOS DEBEN TERNER LA MISMA BASE DE DATOS Y CAMBIAR El NOMBRES DE USUARIO PARA QUE SIRVA EN SU MAQUINA  
 try:
     # conexion a la Base de Datos 
-    connection = pyodbc.connect('DRIVER={SQL Server};SERVER=DESKTOP-QSDIA4P\MSSQLSERVER01;DATABASE=PruebaProyecto;Trusted_Connection=yes;')
+    connection = pyodbc.connect('DRIVER={SQL Server};SERVER=DESKTOP-QSDIA4P\MSSQLSERVER01;DATABASE=GrupoTalse;Trusted_Connection=yes;')
     # db es el objeto para las ejecuciones de la base de datos 
     db = connection.cursor()
-
     """  # Ejecucion de Query de prueba 
     rows = db.execute("SELECT * FROM Tabla1")
     #Impresion 
@@ -25,13 +24,17 @@ app.secret_key = 'EdwingMaricon'  # Reemplaza con una clave secreta segura
 def login():
     session.clear()
     if request.method == 'POST':
-        usuario = request.form['usuario']
-        contraseña = request.form['contraseña']
+        usuario = request.form.get['usuario']
+        contrasena = request.form.get['contrasena']
 
-        # Reemplazar esto con tu lógica de autenticación
-        if usuario == 'tu_usuario' and contraseña == '123':
-            session['usuario'] = usuario
-            return redirect("index")
+        # Buscar los datos en la base de datos en la tabla de administradores 
+        rows = db.execute("SELECT * FROM users WHERE username = :username AND hash = :hash", {"usuario": usuario, "hash": contrasena}).fetchall()
+        if len(rows) == 1:
+            session["id"] = rows[0]["id"]
+            return render_template("index.html")
+        else:
+            flash('Contrasena o usuario equivocada vuelva a intentarlo')
+            return redirect(url_for('login'))
     else:
         return render_template('login.html') 
     
@@ -39,10 +42,43 @@ def login():
 def index():
     if request.method == "GET":
         # Ejecucion de Query de prueba 
-        db.execute("SELECT * FROM Tabla1")
-        rows =  db.fetchall
-        columns = [column[0] for column in db.description]
+        #db.execute("SELECT * FROM Tabla1")
+        # rows =  db.fetchall
+        # columns = [column[0] for column in db.description]
         
-        rows = [dict(zip(columns, row)) for row in db.fetchall()]
+        """ CHAT GPT
+        # Ejecuta la consulta SQL
+        db.execute("SELECT * FROM Tabla1")
+
+        # Obtiene las filas y las columnas
+        rows = db.fetchall()
+        columns = [column[0] for column in db.description]
+
+        # Convierte las filas a una lista de diccionarios
+        rows = [dict(zip(columns, row)) for row in rows]
+
+        # Pasa los datos a la plantilla HTML para ser renderizados
         return render_template('index.html', rows=rows)
+        """
+
+        #rows = [dict(zip(columns, row)) for row in db.fetchall()]
+        #return render_template('index.html', rows=rows)
     
+@app.route("/logout", methods = ['GET', 'POST'])
+def logout():
+    # cerramos sesion 
+    session.clear()
+    # redireccionamos al login 
+    return redirect("/")
+
+@app.route("/agregar_prestamo", methods = ['GET', 'POST'])
+def agregar_prestamo():
+    return "AGREGAR PRESTAMO"
+
+@app.route("/Historial", methods = ['GET', 'POST'])
+def Historial():
+    return "Historial Historial"
+
+@app.route("/Prestamo_Detallado", methods = ['GET', 'POST'])
+def Historial():
+    return "Prestamo_Detallado"
